d5051b91988c7becbd087925d53516cd816b9b43
Roman <R-Tupac@yandex.ru>
2023-02-01 01:14:42 +0300

    added homework-3. correction

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -6,9 +6,11 @@ const tabContent = [...document.getElementsByClassName("tab__content")];
 function tabClick(e) {
     // указываем на обьект и добавляем активный класс таба
     e.currentTarget.classList.add("tab_active");
-    // на активных табов удаляем класс
-    document.querySelector(".tab_active").classList.remove("tab_active");
-    document.querySelector(".tab__content_active").classList.remove("tab__content_active");
+    // на активных табов удаляем класс c проверкой является ли возвращаемый объект null
+    const activeTab = document.querySelector(".tab_active");
+    if (activeTab) { activeTab.classList.remove("tab_active"); }
+    const activeTabContent = document.querySelector(".tab__content_active");
+    if (activeTabContent) { activeTabContent.classList.remove("tab__content_active"); }
     // на таб контент делаем связку с табом и делаем класс активным
     tabContent[tab.indexOf(e.currentTarget)].classList.add("tab__content_active");
 }
