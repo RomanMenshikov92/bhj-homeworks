d182171d58f05e696ef9193437883272f8cfbc97
Roman Menshikov <R-Tupac@yandex.ru>
2023-02-17 21:24:00 +0300

    added homework-3. correction

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,19 +1,24 @@
-// объявление переменных 
+// объявление переменных
 const tab = [...document.getElementsByClassName("tab")];
 const tabContent = [...document.getElementsByClassName("tab__content")];
 
 // функция клика на таб
 function tabClick(e) {
-    // указываем на обьект и добавляем активный класс таба
-    e.currentTarget.classList.add("tab_active");
-    // на активных табов удаляем класс c проверкой является ли возвращаемый объект null
-    const activeTab = document.querySelector(".tab_active");
-    if (activeTab) { activeTab.classList.remove("tab_active"); }
-    const activeTabContent = document.querySelector(".tab__content_active");
-    if (activeTabContent) { activeTabContent.classList.remove("tab__content_active"); }
-    // на таб контент делаем связку с табом и делаем класс активным
-    tabContent[tab.indexOf(e.currentTarget)].classList.add("tab__content_active");
+  // на активных табов удаляем класс c проверкой является ли возвращаемый объект null
+  const activeTab = document.querySelector(".tab_active");
+  if (activeTab) {
+    activeTab.classList.remove("tab_active");
+  }
+  const activeTabContent = document.querySelector(".tab__content_active");
+  if (activeTabContent) {
+    activeTabContent.classList.remove("tab__content_active");
+  }
+
+  // указываем на обьект и добавляем активный класс таба
+  e.currentTarget.classList.add("tab_active");
+  // на таб контент делаем связку с табом и делаем класс активным
+  tabContent[tab.indexOf(e.currentTarget)].classList.add("tab__content_active");
 }
 
 // перебираем элементы и вешаем событие таба
-tab.forEach(element => element.addEventListener("click", tabClick));
+tab.forEach((element) => element.addEventListener("click", tabClick));
